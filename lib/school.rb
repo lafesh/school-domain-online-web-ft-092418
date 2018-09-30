c03c375a19b7fe56a9623fba97f8108ae8bb9f9d
Gina Fedder <ginafedder@yahoo.com>
2018-09-30 01:58:56 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,1 @@
 # code here!
-class School
-  attr_reader :name, :roster
-  
-  def initialize(name)
-    @name = name 
-    @roster = {}
-  end 
-  
-  def add_student(name, grade)
-    @roster[grade] = [] unless @roster[grade].include?(grade)
-    @roster[grade] << name
-  end 
-end 
