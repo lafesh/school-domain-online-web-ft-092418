779c669c3236fd70a8b5cbe30d3d1ebe6376ffc5
Gina Fedder <ginafedder@yahoo.com>
2018-09-30 04:40:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,22 +3,22 @@ class School
  attr_reader :name, :roster
 
   def initialize(name)
-   @name = name
-   @roster = {}
+    @name = name
+    @roster = {}
   end
   
   def add_student(name, grade)
-  @roster[grade] ||= []
-   @roster[grade] << name
+    @roster[grade] ||= []
+    @roster[grade] << name
   end
   
   def grade(grade)
-     return @roster[grade]
+    return @roster[grade]
   end
   
   def sort
-   hash = {}
-   @roster.each { |key, value|  hash[key] = value.sort }
+    hash = {}
+    @roster.each { |key, value|  hash[key] = value.sort }
     hash
    end
 end 
